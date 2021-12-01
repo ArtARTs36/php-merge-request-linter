d55418f6edcddab530af15ec43d6331ed7a81c71
ArtARTs36 <temicska99@mail.ru>
2021-12-01 23:31:23 +0300

    add test

---
+++ tests/Unit/Linter/LintResultTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Linter;
+
+use ArtARTs36\MergeRequestLinter\Linter\LintResult;
+use ArtARTs36\MergeRequestLinter\Tests\Mocks\EmptyNote;
+use ArtARTs36\MergeRequestLinter\Tests\TestCase;
+
+final class LintResultTest extends TestCase
+{
+    public function providerForTestIsFail(): array
+    {
+        return [
+            [
+                LintResult::bad(new EmptyNote(), 0.12),
+                true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerForTestIsFail
+     * @covers \ArtARTs36\MergeRequestLinter\Tests\Mocks\EmptyNote::getDescription
+     */
+    public function testIsFail(LintResult $result, bool $expected): void
+    {
+        self::assertEquals($expected, $result->isFail());
+    }
+}
