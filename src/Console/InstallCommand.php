0e8950004c742eb2fa0e0d96e9bc568f6fd38c3b
ArtARTs36 <temicska99@mail.ru>
2021-12-01 14:07:28 +0300

    add install command

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InstallCommand extends Command
+{
+    protected static $defaultName = 'install';
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dir = getcwd();
+
+        copy(__DIR__ . '/../../.mr-linter.php', $dir . '/amr-linter.php');
+
+        return self::SUCCESS;
+    }
+}
