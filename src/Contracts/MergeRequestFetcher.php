8d41ca2756e419a828c9433dc235603de51d7a95
ArtARTs36 <temicska99@mail.ru>
2021-12-02 00:40:25 +0300

    add throws

--- src/Contracts/MergeRequestFetcher.php
+++ src/Contracts/MergeRequestFetcher.php
@@ -2,6 +2,7 @@
 
 namespace ArtARTs36\MergeRequestLinter\Contracts;
 
+use ArtARTs36\MergeRequestLinter\Exception\InvalidCredentialsException;
 use ArtARTs36\MergeRequestLinter\Request\MergeRequest;
 use OndraM\CiDetector\Ci\CiInterface;
 
@@ -12,6 +13,7 @@ interface MergeRequestFetcher
 {
     /**
      * Fetch merge request of CI
+     * @throws InvalidCredentialsException
      */
     public function fetch(CiInterface $ci): MergeRequest;
 }
