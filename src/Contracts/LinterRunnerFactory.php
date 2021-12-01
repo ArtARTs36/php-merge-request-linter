93bc5c30465b02539e75c7f830980f50b9634f83
ArtARTs36 <temicska99@mail.ru>
2021-12-01 14:28:46 +0300

    up

--- src/Contracts/LinterRunnerFactory.php
+++ src/Contracts/LinterRunnerFactory.php
@@ -5,7 +5,13 @@ namespace ArtARTs36\MergeRequestLinter\Contracts;
 use ArtARTs36\MergeRequestLinter\Configuration\Config;
 use ArtARTs36\MergeRequestLinter\Linter\LinterRunner;
 
+/**
+ * Factory for LinterRunner
+ */
 interface LinterRunnerFactory
 {
+    /**
+     * Create LinterRunner instance
+     */
     public function create(Config $config): LinterRunner;
 }
