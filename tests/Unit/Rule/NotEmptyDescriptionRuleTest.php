7e422abec68086d37ce799477fc891ba4c0281b4
ArtARTs36 <temicska99@mail.ru>
2021-12-01 22:24:20 +0300

    Add test

---
+++ tests/Unit/Rule/NotEmptyDescriptionRuleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Rule;
+
+use ArtARTs36\MergeRequestLinter\Request\MergeRequest;
+use ArtARTs36\MergeRequestLinter\Rule\NotEmptyDescriptionRule;
+use ArtARTs36\MergeRequestLinter\Tests\TestCase;
+
+final class NotEmptyDescriptionRuleTest extends TestCase
+{
+    public function providerForTestLint(): array
+    {
+        return [
+            [
+                $this->makeMergeRequest(),
+                true,
+            ],
+            [
+                $this->makeMergeRequest([
+                    'description' => 'Test',
+                ]),
+                false,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerForTestLint
+     * @covers \ArtARTs36\MergeRequestLinter\Rule\NotEmptyDescriptionRule::lint
+     */
+    public function testLint(MergeRequest $request, bool $hasNotes): void
+    {
+        self::assertEquals($hasNotes, count((new NotEmptyDescriptionRule())->lint($request)) > 0);
+    }
+}
