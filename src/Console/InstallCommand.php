135bf25a771b18b61ec638f9b87959a717f9a194
ArtARTs36 <temicska99@mail.ru>
2021-12-02 15:07:48 +0300

    update InstallCommand

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -5,6 +5,7 @@ namespace ArtARTs36\MergeRequestLinter\Console;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class InstallCommand extends Command
 {
@@ -14,7 +15,11 @@ class InstallCommand extends Command
     {
         $dir = getcwd();
 
-        copy(__DIR__ . '/../../.mr-linter.php', $dir . '/mr-linter.php');
+        copy(__DIR__ . '/../../.mr-linter.php', $pathTo = $dir . '/.mr-linter.php');
+
+        $style = new SymfonyStyle($input, $output);
+
+        $style->info('Was copied configuration file to: '. $pathTo);
 
         return self::SUCCESS;
     }
