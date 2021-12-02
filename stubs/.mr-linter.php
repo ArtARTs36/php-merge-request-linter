9bc6a96d99e4782056ab3edee9a5178dcb9901e6
ArtARTs36 <temicska99@mail.ru>
2021-12-03 02:25:43 +0300

    update configs

--- stubs/.mr-linter.php
+++ stubs/.mr-linter.php
@@ -3,11 +3,11 @@
 use ArtARTs36\MergeRequestLinter\Ci\Credentials\Token;
 use ArtARTs36\MergeRequestLinter\Ci\System\GithubActions;
 use ArtARTs36\MergeRequestLinter\Ci\System\GitlabCi;
+use ArtARTs36\MergeRequestLinter\Contracts\Environment;
 use ArtARTs36\MergeRequestLinter\Rule\HasAnyLabelsOfRule;
 use ArtARTs36\MergeRequestLinter\Rule\HasAnyLabelsRule;
 use ArtARTs36\MergeRequestLinter\Rule\DescriptionNotEmptyRule;
 use ArtARTs36\MergeRequestLinter\Rule\TitleStartsWithAnyPrefixRule;
-use ArtARTs36\MergeRequestLinter\Rule\WhenHasLabelMustDescriptionContainsLinkOfAnyDomainsRule;
 
 return [
     'rules' => [
@@ -21,11 +21,10 @@ return [
             '[Bug]',
             '[Docs]',
         ]),
-        WhenHasLabelMustDescriptionContainsLinkOfAnyDomainsRule::make('aa', ['bb']),
     ],
     'credentials' => [
         GitlabCi::class => new Token(getenv('GITLAB_HTTP_TOKEN')),
         GithubActions::class => new Token(getenv('GITHUB_HTTP_TOKEN')),
     ],
-    'http_client' => fn () => new \GuzzleHttp\Client(),
+    'http_client' => fn (string $ciName, Environment $environment, string $ciSystemClass) => new \GuzzleHttp\Client(),
 ];
