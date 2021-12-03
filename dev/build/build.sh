d416c79fc14221d4f1a829d643370df81d78ea7b
ArtARTs36 <temicska99@mail.ru>
2021-12-03 03:33:07 +0300

    update phar building

--- dev/build/build.sh
+++ dev/build/build.sh
@@ -1,6 +1,14 @@
+# Library without dev deps
 composer update --no-interaction --no-progress --no-dev --prefer-stable --optimize-autoloader
 
+# Build box
+cd dev/build/ && composer install
+
+# cd to home
+
+cd ../../
+
+# Compile phar
 dev/build/vendor/bin/box compile
 
-git checkout composer.json
 composer update --no-interaction --no-progress -q
