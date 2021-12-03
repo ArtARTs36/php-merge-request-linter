9151d6d0ab9aea51768a3aaf4f8653ded8b165ba
ArtARTs36 <temicska99@mail.ru>
2021-12-03 10:57:55 +0300

    Update PHAR Building

--- dev/build/build.sh
+++ dev/build/build.sh
@@ -8,8 +8,12 @@ cd dev/build/ && composer install
 
 cd ../../
 
+echo "y" | composer require guzzlehttp/guzzle
+
 # Compile phar
 dev/build/vendor/bin/box compile
+
+git checkout composer.json
 
 if [ -z ${NOT_UPDATE_AFTER_BUILD} ]; then
   composer update --no-interaction --no-progress -q;
