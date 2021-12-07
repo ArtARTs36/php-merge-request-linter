140ae4ff0a7e17c10c36a0d4320ee10bd94c79bc
ArtARTs36 <temicska99@mail.ru>
2021-12-07 16:31:50 +0300

    Allow array of Rule in OnTargetBranchRule

--- src/Rule/OnTargetBranchRule.php
+++ src/Rule/OnTargetBranchRule.php
@@ -2,6 +2,7 @@
 
 namespace ArtARTs36\MergeRequestLinter\Rule;
 
+use ArtARTs36\MergeRequestLinter\Contracts\Note;
 use ArtARTs36\MergeRequestLinter\Contracts\Rule;
 use ArtARTs36\MergeRequestLinter\Contracts\RuleDefinition;
 use ArtARTs36\MergeRequestLinter\Request\MergeRequest;
@@ -11,20 +12,40 @@ use ArtARTs36\MergeRequestLinter\Request\MergeRequest;
  */
 class OnTargetBranchRule implements Rule
 {
-    public function __construct(protected string $targetBranch, protected Rule $decorateRule)
+    /**
+     * @var \ArtARTs36\MergeRequestLinter\Contracts\Rule[]
+     */
+    protected array $decorateRules;
+
+    /**
+     * @param array<Rule>|Rule $decorateRule
+     */
+    public function __construct(protected string $targetBranch, array|Rule $decorateRule)
     {
-        //
+        $this->decorateRules = is_array($decorateRule) ? $decorateRule : [$decorateRule];
     }
 
     public function lint(MergeRequest $request): array
     {
-        return $request->targetBranch->equals($this->targetBranch) ?
-            $this->decorateRule->lint($request) :
-            [];
+        return $request->targetBranch->equals($this->targetBranch) ? $this->runLintOnDecorateRules($request) : [];
     }
 
     public function getDefinition(): RuleDefinition
     {
         return new Definition('Apply another rule if the target branch equals: ' . $this->targetBranch);
+    }
+
+    /**
+     * @return array<Note>
+     */
+    protected function runLintOnDecorateRules(MergeRequest $request): array
+    {
+        $notes = [];
+
+        foreach ($this->decorateRules as $rule) {
+            $notes[] = $rule->lint($request);
+        }
+
+        return array_filter($notes);
     }
 }
