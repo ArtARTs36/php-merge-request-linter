088353c9b0d694a97c9f258ce5ae9b149e7bb568
artarts36 <temicska99@mail.ru>
2022-12-07 20:10:09 +0300

    Add config loader proxy

---
+++ src/Configuration/ConfigLoaderProxy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Configuration;
+
+use ArtARTs36\MergeRequestLinter\Contracts\ConfigLoader;
+
+class ConfigLoaderProxy implements ConfigLoader
+{
+    private ?ConfigLoader $loader;
+
+    /**
+     * @param \Closure(): ConfigLoader $factory
+     */
+    public function __construct(
+        private \Closure $factory,
+    ) {
+        //
+    }
+
+    public function load(string $path): Config
+    {
+        return $this->retrieve()->load($path);
+    }
+
+    private function retrieve(): ConfigLoader
+    {
+        if ($this->loader === null) {
+            $factory = $this->factory;
+
+            $this->loader = $factory();
+        }
+
+        return $this->loader;
+    }
+}
