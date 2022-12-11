7dd72ee24516fb832d87fc0992b26aa0e3b05b40
artarts36 <temicska99@mail.ru>
2022-12-11 15:09:06 +0300

    fix test

--- tests/Mocks/EmptyNote.php
+++ tests/Mocks/EmptyNote.php
@@ -3,9 +3,15 @@
 namespace ArtARTs36\MergeRequestLinter\Tests\Mocks;
 
 use ArtARTs36\MergeRequestLinter\Note\AbstractNote;
+use ArtARTs36\MergeRequestLinter\Note\NoteColor;
 
 final class EmptyNote extends AbstractNote
 {
+    public function getColor(): NoteColor
+    {
+        return NoteColor::WHITE;
+    }
+
     public function getDescription(): string
     {
         return '';
