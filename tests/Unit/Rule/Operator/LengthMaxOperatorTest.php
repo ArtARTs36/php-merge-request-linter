0d3d1ddd58f5c638aa29da3d151fee75d7e3918c
artarts36 <temicska99@mail.ru>
2022-12-13 01:10:12 +0300

    LengthMaxOperatorTest

---
+++ tests/Unit/Rule/Operator/LengthMaxOperatorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Rule\Operator;
+
+use ArtARTs36\MergeRequestLinter\Rule\Condition\LengthMaxOperator;
+use ArtARTs36\MergeRequestLinter\Tests\Mocks\MockPropertyExtractor;
+use ArtARTs36\MergeRequestLinter\Tests\TestCase;
+
+final class LengthMaxOperatorTest extends TestCase
+{
+    public function providerForEvaluate(): array
+    {
+        return [
+            [
+                'title',
+                5,
+                true,
+            ],
+            [
+                'title',
+                3,
+                false,
+            ],
+        ];
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Rule\Condition\LengthMaxOperator::evaluate
+     * @dataProvider providerForEvaluate
+     */
+    public function testEvaluate(string $propertyValue, int $min, bool $expected): void
+    {
+        $operator = new LengthMaxOperator(new MockPropertyExtractor($propertyValue), 'prop', $min);
+
+        self::assertEquals($expected, $operator->evaluate($this->makeMergeRequest()));
+    }
+}
