41b6a6d584bb8f4bdfac989f77275fc5962bbad3
artarts36 <temicska99@mail.ru>
2022-12-13 13:03:33 +0300

    clean

--- src/Rule/Condition/EndsOperator.php
+++ src/Rule/Condition/EndsOperator.php
@@ -5,24 +5,15 @@ namespace ArtARTs36\MergeRequestLinter\Rule\Condition;
 use ArtARTs36\MergeRequestLinter\Contracts\ConditionOperator;
 use ArtARTs36\MergeRequestLinter\Request\MergeRequest;
 use ArtARTs36\Str\Facade\Str;
-use ArtARTs36\MergeRequestLinter\Contracts\PropertyExtractor;
 
-class EndsOperator extends AbstractOperator implements ConditionOperator
+class EndsOperator extends AbstractScalarOperator implements ConditionOperator
 {
     public const NAME = 'ends';
-
-    public function __construct(
-        PropertyExtractor $propertyExtractor,
-        string $property,
-        private mixed $ends,
-    ) {
-        parent::__construct($propertyExtractor, $property);
-    }
 
     protected function doEvaluate(MergeRequest $request): bool
     {
         $value = $this->propertyExtractor->scalar($request, $this->property);
 
-        return Str::endsWith("$value", $this->ends);
+        return Str::endsWith("$value", $this->value);
     }
 }
--- src/Rule/Condition/StartsOperator.php
+++ src/Rule/Condition/StartsOperator.php
@@ -5,24 +5,15 @@ namespace ArtARTs36\MergeRequestLinter\Rule\Condition;
 use ArtARTs36\MergeRequestLinter\Contracts\ConditionOperator;
 use ArtARTs36\MergeRequestLinter\Request\MergeRequest;
 use ArtARTs36\Str\Facade\Str;
-use ArtARTs36\MergeRequestLinter\Contracts\PropertyExtractor;
 
-class StartsOperator extends AbstractOperator implements ConditionOperator
+class StartsOperator extends AbstractScalarOperator implements ConditionOperator
 {
     public const NAME = 'starts';
-
-    public function __construct(
-        PropertyExtractor $propertyExtractor,
-        string $property,
-        private mixed $starts,
-    ) {
-        parent::__construct($propertyExtractor, $property);
-    }
 
     protected function doEvaluate(MergeRequest $request): bool
     {
         $value = $this->propertyExtractor->scalar($request, $this->property);
 
-        return Str::startsWith("$value", $this->starts);
+        return Str::startsWith("$value", $this->value);
     }
 }
