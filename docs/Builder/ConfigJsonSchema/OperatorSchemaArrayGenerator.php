f24f7de40d6da9c001bd9695a87588f30b304936
artarts36 <temicska99@mail.ru>
2022-12-17 02:10:45 +0300

    scan objects

--- docs/Builder/ConfigJsonSchema/OperatorSchemaArrayGenerator.php
+++ docs/Builder/ConfigJsonSchema/OperatorSchemaArrayGenerator.php
@@ -3,7 +3,9 @@
 namespace ArtARTs36\MergeRequestLinter\DocBuilder\ConfigJsonSchema;
 
 use ArtARTs36\MergeRequestLinter\Attribute\SupportsConditionOperator;
+use ArtARTs36\MergeRequestLinter\Contracts\ConditionOperator;
 use ArtARTs36\MergeRequestLinter\Request\MergeRequest;
+use ArtARTs36\MergeRequestLinter\Support\DataStructure\Set;
 use ArtARTs36\Str\Str;
 
 class OperatorSchemaArrayGenerator
@@ -23,8 +25,33 @@ class OperatorSchemaArrayGenerator
 
         $operatorMetadata = $this->operatorMetadataLoader->load();
 
+        return $this->doGenerate($reflector, $operatorMetadata, $opArray, '');
+    }
+
+    /**
+     * @param array<class-string<ConditionOperator>, OperatorMetadata> $operatorMetadata
+     */
+    private function doGenerate(\ReflectionClass $reflector, array $operatorMetadata, array $opArray, string $propertyPrefix): array
+    {
         foreach ($reflector->getProperties() as $property) {
-            $opArray['properties'][$property->getName()] = [
+            $propertyName = $property->getName();
+
+            if (\ArtARTs36\Str\Facade\Str::isNotEmpty($propertyPrefix)) {
+                $propertyName = $propertyPrefix . '.' . $propertyName;
+            }
+
+            if (class_exists($property->getType()->getName()) && $this->allowObjectScan($property->getType()->getName())) {
+                $opArray = array_merge($opArray, $this->doGenerate(
+                    new \ReflectionClass($property->getType()->getName()),
+                    $operatorMetadata,
+                    $opArray,
+                    $propertyName,
+                ));
+
+                continue;
+            }
+
+            $opArray['properties'][$propertyName] = [
                 'type' => 'object',
                 'properties' => [],
             ];
@@ -36,7 +63,7 @@ class OperatorSchemaArrayGenerator
                     $operatorMeta = $operatorMetadata[$operatorClass];
 
                     if ($operatorMeta->evaluatesSameType) {
-                        $opArray['properties'][$property->getName()]['properties'][$operatorMeta->name] = [
+                        $opArray['properties'][$propertyName]['properties'][$operatorMeta->name] = [
                             'type' => $this->prepareTypeToPrimitive($property->getType()->getName()),
                         ];
 
@@ -46,7 +73,7 @@ class OperatorSchemaArrayGenerator
                     $opParamTypes = $operatorMeta->allowValueTypes;
 
                     if (count($opParamTypes) === 1) {
-                        $opArray['properties'][$property->getName()]['properties'][$operatorMeta->name] = [
+                        $opArray['properties'][$propertyName]['properties'][$operatorMeta->name] = [
                             'type' => $opParamTypes[0],
                         ];
                     } else {
@@ -58,13 +85,18 @@ class OperatorSchemaArrayGenerator
                             ];
                         }
 
-                        $opArray['properties'][$property->getName()]['properties'][$operatorMeta->name]['anyOf'] = $anyOf;
+                        $opArray['properties'][$propertyName]['properties'][$operatorMeta->name]['anyOf'] = $anyOf;
                     }
                 }
             }
         }
 
         return $opArray;
+    }
+
+    private function allowObjectScan(string $type): bool
+    {
+        return $type !== Set::class && $type !== Str::class;
     }
 
     private function prepareTypeToPrimitive(string $type): string
