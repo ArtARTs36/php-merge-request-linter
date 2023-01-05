4853a44fe9ee2ec1d471edea5b3f1ed0dead4c8f
artarts36 <temicska99@mail.ru>
2023-01-05 05:54:00 +0300

    clean

--- src/Ci/Credentials/Token.php
+++ src/Ci/Credentials/Token.php
@@ -2,7 +2,17 @@
 
 namespace ArtARTs36\MergeRequestLinter\Ci\Credentials;
 
-final class Token extends AbstractCredentials
+use ArtARTs36\MergeRequestLinter\Contracts\RemoteCredentials;
+
+final class Token implements RemoteCredentials
 {
-    //
+    public function __construct(protected string $token)
+    {
+        //
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
 }
