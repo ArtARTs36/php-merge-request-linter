9b0f99aefa4e0d4fdd0b0ccc140ab5936c9ba28a
Artem Ukrainskiy <temicska99@mail.ru>
2023-01-08 10:36:35 +0300

    Add YAML Config Loader (#13)

---
+++ src/Configuration/Loader/ArrayConfigLoaderFactory.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Configuration\Loader;
+
+use ArtARTs36\FileSystem\Contracts\FileSystem;
+use ArtARTs36\MergeRequestLinter\CI\System\DefaultSystems;
+use ArtARTs36\MergeRequestLinter\Condition\Evaluator\DefaultEvaluators;
+use ArtARTs36\MergeRequestLinter\Condition\Evaluator\EvaluatorFactory;
+use ArtARTs36\MergeRequestLinter\Condition\Operator\OperatorFactory;
+use ArtARTs36\MergeRequestLinter\Condition\Operator\OperatorResolver;
+use ArtARTs36\MergeRequestLinter\Configuration\Value\EnvTransformer;
+use ArtARTs36\MergeRequestLinter\Configuration\Value\FileTransformer;
+use ArtARTs36\MergeRequestLinter\Contracts\Environment\Environment;
+use ArtARTs36\MergeRequestLinter\Rule\DefaultRules;
+use ArtARTs36\MergeRequestLinter\Rule\Factory\Argument\Builder;
+use ArtARTs36\MergeRequestLinter\Rule\Factory\Argument\DefaultResolvers;
+use ArtARTs36\MergeRequestLinter\Rule\Factory\Constructor\ConstructorFinder;
+use ArtARTs36\MergeRequestLinter\Rule\Factory\Resolver;
+use ArtARTs36\MergeRequestLinter\Rule\Factory\RuleFactory;
+use ArtARTs36\MergeRequestLinter\Support\Reflector\CallbackPropertyExtractor;
+
+class ArrayConfigLoaderFactory
+{
+    public const SUPPORT_FORMATS = [
+        'json' => JsonConfigLoader::class,
+        'yaml' => YamlConfigLoader::class,
+        'yml' => YamlConfigLoader::class,
+    ];
+
+    public function __construct(
+        private FileSystem $fileSystem,
+        private Environment $environment,
+    ) {
+        //
+    }
+
+    /**
+     * @param string&key-of<self::SUPPORT_FORMATS> $format
+     */
+    public function create(string $format): AbstractArrayConfigLoader
+    {
+        if (! array_key_exists($format, self::SUPPORT_FORMATS)) {
+            throw new \InvalidArgumentException(sprintf(
+                'ConfigLoader for format "%s" not found. Expected one of: [%s]',
+                $format,
+                implode(', ', array_keys(self::SUPPORT_FORMATS)),
+            ));
+        }
+
+        $loaderClass = self::SUPPORT_FORMATS[$format];
+
+        $ruleFactory = new RuleFactory(
+            new Builder(
+                DefaultResolvers::get(),
+            ),
+            new ConstructorFinder(),
+        );
+
+        $operatorFactory = new OperatorFactory(new CallbackPropertyExtractor(), new EvaluatorFactory(
+            DefaultEvaluators::map(),
+        ));
+
+        return new $loaderClass(
+            $this->fileSystem,
+            new CredentialMapper(
+                [
+                    new EnvTransformer($this->environment),
+                    new FileTransformer($this->fileSystem),
+                ],
+                DefaultSystems::map(),
+            ),
+            new RulesMapper(
+                new Resolver(DefaultRules::map(), $ruleFactory, new OperatorResolver($operatorFactory)),
+            ),
+        );
+    }
+}
