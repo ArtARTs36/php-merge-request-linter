a48288774742b2aec6b3b0cb218a7eaa4fdc19ec
artarts36 <temicska99@mail.ru>
2023-01-14 15:23:06 +0300

    fix lint

--- src/Contracts/Text/TextDecoder.php
+++ src/Contracts/Text/TextDecoder.php
@@ -4,6 +4,9 @@ namespace ArtARTs36\MergeRequestLinter\Contracts\Text;
 
 use ArtARTs36\MergeRequestLinter\Exception\TextDecodingException;
 
+/**
+ * Interface for decoding text.
+ */
 interface TextDecoder
 {
     /**
