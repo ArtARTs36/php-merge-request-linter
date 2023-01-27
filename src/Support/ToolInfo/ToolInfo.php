b47f5f22d75d60b2b4b2854f2e887166c7203751
artarts36 <temicska99@mail.ru>
2023-01-27 13:37:18 +0300

    clean

--- src/Support/ToolInfo/ToolInfo.php
+++ src/Support/ToolInfo/ToolInfo.php
@@ -13,6 +13,8 @@ class ToolInfo
     public const REPO_NAME = 'php-merge-request-linter';
     public const REPO_URI = 'https://github.com/ArtARTs36/php-merge-request-linter';
 
+    private const PHAR_URI_PREFIX = 'phar://';
+
     public function __construct(
         private readonly GithubClient $github,
     ) {
@@ -21,13 +23,15 @@ class ToolInfo
 
     public function getLatestVersion(): ?Tag
     {
-        $versions = $this->github->getTags(new TagsInput(self::REPO_OWNER, self::REPO_NAME));
-
-        return $versions->sortByMajority()->first();
+        return $this
+            ->github
+            ->getTags(new TagsInput(self::REPO_OWNER, self::REPO_NAME))
+            ->sortByMajority()
+            ->first();
     }
 
     public function usedAsPhar(): bool
     {
-        return Str::startsWith(__DIR__, 'phar://');
+        return Str::startsWith(__DIR__, self::PHAR_URI_PREFIX);
     }
 }
