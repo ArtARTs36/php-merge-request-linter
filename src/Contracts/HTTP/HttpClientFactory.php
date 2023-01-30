60d388fe7793ad89652b8ad1e46eaabf685ffb66
artarts36 <temicska99@mail.ru>
2023-01-30 17:52:55 +0300

    Move 'HttpClientTypeNotSupported' exception

--- src/Contracts/HTTP/HttpClientFactory.php
+++ src/Contracts/HTTP/HttpClientFactory.php
@@ -3,6 +3,7 @@
 namespace ArtARTs36\MergeRequestLinter\Contracts\HTTP;
 
 use ArtARTs36\MergeRequestLinter\Configuration\HttpClientConfig;
+use ArtARTs36\MergeRequestLinter\Exception\HttpClientTypeNotSupported;
 
 /**
  * Interface for HTTP Client Factory.
@@ -11,6 +12,7 @@ interface HttpClientFactory
 {
     /**
      * Create HTTP Client.
+     * @throws HttpClientTypeNotSupported
      */
     public function create(HttpClientConfig $config): Client;
 }
--- src/Support/Http/NullClient.php
+++ src/Support/Http/NullClient.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace ArtARTs36\MergeRequestLinter\Tests\Mocks;
+namespace ArtARTs36\MergeRequestLinter\Support\Http;
 
 use ArtARTs36\MergeRequestLinter\Contracts\HTTP\Client;
+use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -10,7 +11,7 @@ final class NullClient implements Client
 {
     public function sendRequest(RequestInterface $request): ResponseInterface
     {
-        //
+        return new Response();
     }
 
     public function sendAsyncRequests(array $requests): array
