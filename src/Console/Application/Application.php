6def6f1d5e5be498113936abeb9f0fe172d3d7a2
artarts36 <temicska99@mail.ru>
2023-01-31 22:21:36 +0300

    update version

--- src/Console/Application/Application.php
+++ src/Console/Application/Application.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends \Symfony\Component\Console\Application
 {
-    public const VERSION = '0.7.4';
+    public const VERSION = '0.7.5';
 
     public function __construct(
         private readonly MetricManager $metrics,
