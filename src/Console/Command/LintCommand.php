3d2b43f9ec077f1cbbd49865b9997e947a7bd471
artarts36 <temicska99@mail.ru>
2023-02-04 00:49:04 +0300

    Add NotePrinter

--- src/Console/Command/LintCommand.php
+++ src/Console/Command/LintCommand.php
@@ -4,20 +4,17 @@ namespace ArtARTs36\MergeRequestLinter\Console\Command;
 
 use ArtARTs36\MergeRequestLinter\Configuration\Config;
 use ArtARTs36\MergeRequestLinter\Console\Interaction\LintSubscriber;
+use ArtARTs36\MergeRequestLinter\Console\Presentation\NotePrinter;
 use ArtARTs36\MergeRequestLinter\Contracts\Config\ConfigResolver;
 use ArtARTs36\MergeRequestLinter\Contracts\Linter\LinterRunnerFactory;
-use ArtARTs36\MergeRequestLinter\Contracts\Linter\Note;
 use ArtARTs36\MergeRequestLinter\Contracts\Report\MetricManager;
 use ArtARTs36\MergeRequestLinter\IO\Console\ConsolePrinter;
 use ArtARTs36\MergeRequestLinter\IO\Console\SymfonyProgressBar;
+use ArtARTs36\MergeRequestLinter\IO\Console\SymfonyTablePrinter;
 use ArtARTs36\MergeRequestLinter\Linter\Linter;
 use ArtARTs36\MergeRequestLinter\Linter\LintResult;
-use ArtARTs36\MergeRequestLinter\Note\Notes;
-use ArtARTs36\MergeRequestLinter\Note\NoteSeverity;
 use ArtARTs36\MergeRequestLinter\Support\Bytes;
 use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Helper\TableCell;
-use Symfony\Component\Console\Helper\TableCellStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\StyleInterface;
@@ -35,6 +32,7 @@ class LintCommand extends Command
         protected ConfigResolver $config,
         protected LinterRunnerFactory $runnerFactory,
         protected MetricManager $metrics,
+        protected readonly NotePrinter $notePrinter = new NotePrinter(),
     ) {
         parent::__construct();
     }
@@ -89,7 +87,9 @@ class LintCommand extends Command
 
         $style->newLine(2);
 
-        $this->printNotes($style, $result->notes);
+        if (! $result->notes->isEmpty()) {
+            $this->notePrinter->print(new SymfonyTablePrinter($style), $result->notes);
+        }
 
         $this->printMetrics($style, $config->config, $result, $input->getOption('metrics'));
 
@@ -120,38 +120,6 @@ class LintCommand extends Command
         }
 
         $style->table(['Metric', 'Value'], $metrics);
-    }
-
-    private function printNotes(StyleInterface $style, Notes $notes): void
-    {
-        if ($notes->isEmpty()) {
-            return;
-        }
-
-        $table = [];
-
-        $tableCellOptions = [
-            NoteSeverity::Fatal->value => [
-                'style' => new TableCellStyle([
-                    'fg' => 'red',
-                ]),
-            ],
-            NoteSeverity::Normal->value => [],
-        ];
-
-        $counter = 0;
-
-        /** @var Note $note */
-        foreach ($notes as $note) {
-            ++$counter;
-
-            $table[] = [
-                new TableCell("$counter", $tableCellOptions[$note->getSeverity()->value]),
-                new TableCell($note->getDescription(), $tableCellOptions[$note->getSeverity()->value]),
-            ];
-        }
-
-        $style->table(['#', 'Note'], $table);
     }
 
     private function printInfoMessage(OutputInterface $output, string $message): void
