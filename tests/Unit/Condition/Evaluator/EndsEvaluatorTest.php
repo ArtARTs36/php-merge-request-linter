a367e0b98dd4b23a3f68c52b235a28897ef5b388
artarts36 <temicska99@mail.ru>
2023-02-08 21:21:02 +0300

    Add test

---
+++ tests/Unit/Condition/Evaluator/EndsEvaluatorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Condition\Evaluator;
+
+use ArtARTs36\MergeRequestLinter\Condition\Evaluator\EndsEvaluator;
+use ArtARTs36\MergeRequestLinter\Tests\Mocks\MockEvaluatingSubject;
+use ArtARTs36\MergeRequestLinter\Tests\TestCase;
+
+class EndsEvaluatorTest extends TestCase
+{
+    public function providerForTestEvaluate(): array
+    {
+        return [
+            ['Artem', 'em', true],
+            ['Artem1', 'em', false],
+        ];
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Condition\Evaluator\EndsEvaluator::evaluate
+     * @dataProvider providerForTestEvaluate
+     */
+    public function testEvaluate(string $propertyValue, mixed $value, bool $expected): void
+    {
+        $operator = new EndsEvaluator($value);
+
+        self::assertEquals($expected, $operator->evaluate(new MockEvaluatingSubject($propertyValue)));
+    }
+}
