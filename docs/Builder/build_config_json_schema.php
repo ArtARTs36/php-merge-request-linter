4352892c99e2cdddfbe50493b3ae2caddf7e4fc9
artarts36 <temicska99@mail.ru>
2023-02-11 17:40:56 +0300

    up

--- docs/Builder/build_config_json_schema.php
+++ docs/Builder/build_config_json_schema.php
@@ -4,4 +4,12 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 $jsonSchema = new \ArtARTs36\MergeRequestLinter\DocBuilder\ConfigJsonSchema\Generator();
 
-file_put_contents(__DIR__ . '/../../mr-linter-config-schema.json', $jsonSchema->generate()->toJson());
+$path = __DIR__ . '/../../mr-linter-config-schema.json';
+$prevHash = md5_file($path);
+
+file_put_contents($path, $json = $jsonSchema->generate()->toJson());
+
+$updated = $prevHash !== md5($json);
+
+fputs(STDOUT, $updated ? '-> Documentation page updated' : '-> Documentation page is actually');
+fputs(STDOUT, "\n");
