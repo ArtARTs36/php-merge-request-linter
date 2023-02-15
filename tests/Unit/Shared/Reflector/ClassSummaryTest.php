0326f14ff44cca7d6455f5afb9afb89b81d24400
artarts36 <temicska99@mail.ru>
2023-02-15 18:59:24 +0300

    Add ClassSummaryTest

---
+++ tests/Unit/Shared/Reflector/ClassSummaryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Shared\Reflector;
+
+use ArtARTs36\MergeRequestLinter\Shared\Reflector\ClassSummary;
+use ArtARTs36\MergeRequestLinter\Tests\TestCase;
+
+final class ClassSummaryTest extends TestCase
+{
+    public function providerForTestFindInPhpDocComment(): array
+    {
+        return [
+            [
+                '/**
+ * Test summary.
+ */',
+                'Test summary.',
+            ],
+            [
+                '/**
+ * Test summary.
+ * @copyright Name
+ * @package Test
+ */',
+                'Test summary.',
+            ],
+            [
+                '/**
+ * @copyright Name
+ * Test summary.
+ * @package Test
+ */',
+                'Test summary.',
+            ],
+            [
+                '',
+                null,
+            ],
+            [
+                '/**
+ * Test summary.
+ * Test summary2.
+ * @copyright Name
+ * @package Test
+ */',
+                'Test summary.',
+            ],
+        ];
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Reflector\ClassSummary::findInPhpDocComment
+     * @dataProvider providerForTestFindInPhpDocComment
+     */
+    public function testFindInPhpDocComment(string $comment, ?string $expected): void
+    {
+        self::assertEquals($expected, ClassSummary::findInPhpDocComment($comment));
+    }
+}
