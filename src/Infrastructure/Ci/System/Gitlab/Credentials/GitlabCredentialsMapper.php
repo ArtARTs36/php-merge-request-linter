4a454fdf7fd47f7a4e5b3e03ee55c36f9864fcfd
artarts36 <temicska99@mail.ru>
2023-03-12 06:55:45 +0300

    remove token wrap authenticator

--- src/Infrastructure/Ci/System/Gitlab/Credentials/GitlabCredentialsMapper.php
+++ src/Infrastructure/Ci/System/Gitlab/Credentials/GitlabCredentialsMapper.php
@@ -3,7 +3,7 @@
 namespace ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Gitlab\Credentials;
 
 use ArtARTs36\MergeRequestLinter\Domain\CI\Authenticator;
-use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\Credentials\WrapTokenAuthenticator;
+use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\Credentials\TokenAuthenticator;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Contracts\CI\AuthenticatorMapper;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Contracts\Configuration\ConfigValueTransformer;
 
@@ -19,6 +19,6 @@ class GitlabCredentialsMapper implements AuthenticatorMapper
     {
         $token = is_array($credentials) ? reset($credentials) : $credentials;
 
-        return new WrapTokenAuthenticator($this->valueTransformer->tryTransform($token), 'PRIVATE-TOKEN');
+        return new TokenAuthenticator('PRIVATE-TOKEN', $this->valueTransformer->tryTransform($token));
     }
 }
