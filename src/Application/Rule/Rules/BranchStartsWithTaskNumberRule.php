4162915ca1dce8821f1f7d26b6d2e7111e94630f
artarts36 <temicska99@mail.ru>
2023-03-17 02:06:16 +0300

    Add rule

---
+++ src/Application/Rule/Rules/BranchStartsWithTaskNumberRule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ArtARTs36\MergeRequestLinter\Application\Rule\Rules;
+
+use ArtARTs36\MergeRequestLinter\Application\Rule\Definition\Definition;
+use ArtARTs36\MergeRequestLinter\Domain\Request\MergeRequest;
+use ArtARTs36\MergeRequestLinter\Domain\Rule\RuleDefinition;
+
+/**
+ * Source branch must starts with task number of project {projectName}. Mask: {projectName}-number
+ */
+final class BranchStartsWithTaskNumberRule extends AbstractRule
+{
+    public const NAmE = '@mr-linter/branch_starts_with_task_number';
+
+    public function __construct(
+        private readonly string $projectName,
+    ) {
+        //
+    }
+
+    protected function doLint(MergeRequest $request): bool
+    {
+        return $request->sourceBranch->match("/^$this->projectName-\d+/")->isNotEmpty();
+    }
+
+    public function getDefinition(): RuleDefinition
+    {
+        return new Definition(sprintf('Source branch must starts with task number of project "%s"', $this->projectName));
+    }
+}
