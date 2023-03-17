b92bf81fa4aca45d7e3971a2843256e4aada21f6
artarts36 <temicska99@mail.ru>
2023-03-18 02:58:47 +0300

    add test

--- tests/Feature/Console/Command/InstallCommandTest.php
+++ tests/Feature/Console/Command/InstallCommandTest.php
@@ -3,6 +3,7 @@
 namespace ArtARTs36\MergeRequestLinter\Tests\Feature\Console\Command;
 
 use ArtARTs36\MergeRequestLinter\Application\Configuration\Handlers\CreateConfigTaskHandler;
+use ArtARTs36\MergeRequestLinter\Domain\Configuration\ConfigFormat;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Configuration\Copier;
 use ArtARTs36\MergeRequestLinter\Presentation\Console\Command\InstallCommand;
 use ArtARTs36\MergeRequestLinter\Shared\File\Directory;
@@ -12,12 +13,28 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 final class InstallCommandTest extends TestCase
 {
+    public function providerForTestExecute(): array
+    {
+        return [
+            [
+                [],
+                '.mr-linter.yaml',
+            ],
+            [
+                ['--format' => ConfigFormat::JSON->value],
+                '.mr-linter.json',
+            ],
+        ];
+    }
+
     /**
      * @covers \ArtARTs36\MergeRequestLinter\Presentation\Console\Command\InstallCommand::execute
+     * @covers \ArtARTs36\MergeRequestLinter\Presentation\Console\Command\InstallCommand::resolveConfigFormat
      * @covers \ArtARTs36\MergeRequestLinter\Presentation\Console\Command\InstallCommand::__construct
      * @covers \ArtARTs36\MergeRequestLinter\Presentation\Console\Command\InstallCommand::configure
+     * @dataProvider providerForTestExecute
      */
-    public function testExecute(): void
+    public function testExecute(array $options, string $expectedFilename): void
     {
         $cwd = new Cwd();
 
@@ -26,13 +43,13 @@ final class InstallCommandTest extends TestCase
         $command = new InstallCommand(new CreateConfigTaskHandler(new Copier(new Directory(__DIR__ . '/../../../../stubs/'))));
         $tester = new CommandTester($command);
 
-        $tester->execute([]);
+        $tester->execute($options);
 
         $tester->assertCommandIsSuccessful();
-        self::assertFileExists('.mr-linter.yaml');
+        self::assertFileExists($expectedFilename);
         self::assertStringContainsString('Was copied configuration file to:', $tester->getDisplay());
 
-        @unlink('.mr-linter.yaml');
+        @unlink($expectedFilename);
 
         $cwd->revert();
     }
