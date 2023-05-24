92d8d3bc88270753a5ddc87aa44dee4b55d3dbb8
artarts36 <temicska99@mail.ru>
2023-05-24 18:19:24 +0300

    add test for localize

--- tests/Unit/Shared/Time/LocalClockTest.php
+++ tests/Unit/Shared/Time/LocalClockTest.php
@@ -5,10 +5,11 @@ namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Shared\Time;
 use ArtARTs36\MergeRequestLinter\Shared\Time\LocalClock;
 use ArtARTs36\MergeRequestLinter\Tests\TestCase;
 
-final class ClockTest extends TestCase
+final class LocalClockTest extends TestCase
 {
     /**
      * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\LocalClock::now
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\LocalClock::utc
      */
     public function testNow(): void
     {
@@ -23,5 +24,18 @@ final class ClockTest extends TestCase
             $started->format('Y-m-d H:i:s'),
             $result->format('Y-m-d H:i:s'),
         ));
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\LocalClock::localize
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\LocalClock::__construct
+     */
+    public function testLocalize(): void
+    {
+        $clock = new LocalClock($tz = new \DateTimeZone('Europe/Moscow'));
+
+        $date = $clock->localize(new \DateTimeImmutable('now', new \DateTimeZone('UTC')));
+
+        self::assertEquals($tz, $date->getTimezone());
     }
 }
