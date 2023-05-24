2d8494ad7f48a821adae4000cd9a7ab3233cdbee
artarts36 <temicska99@mail.ru>
2023-05-24 12:07:49 +0300

    fix comments

--- src/Shared/Instantiator/InstantiatorFinder.php
+++ src/Shared/Instantiator/InstantiatorFinder.php
@@ -3,12 +3,12 @@
 namespace ArtARTs36\MergeRequestLinter\Shared\Instantiator;
 
 /**
- * Instantiator Finder for class
+ * Instantiator Finder for class.
  */
 interface InstantiatorFinder
 {
     /**
-     * Find constructor for $class
+     * Find constructor for $class.
      * @template T of object
      * @param class-string<T> $class
      * @return Instantiator<T>
