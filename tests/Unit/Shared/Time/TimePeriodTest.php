1b52488cae4e14fd6c12200770d8f49afa8487db
artarts36 <temicska99@mail.ru>
2023-05-25 00:08:00 +0300

    add tests for TimePeriod

--- tests/Unit/Shared/Time/TimePeriodTest.php
+++ tests/Unit/Shared/Time/TimePeriodTest.php
@@ -79,4 +79,64 @@ final class TimePeriodTest extends TestCase
 
         self::assertEquals($expected, $period->input(new \DateTimeImmutable($dateTime)));
     }
+
+    public function providerForTestLte(): array
+    {
+        return [
+            [
+                '01:00',
+                '01:00',
+                true,
+            ],
+            [
+                '01:00',
+                '01:01',
+                true,
+            ],
+            [
+                '01:00',
+                '00:59',
+                false,
+            ],
+        ];
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\Time::lte
+     * @dataProvider providerForTestLte
+     */
+    public function testLte(string $time1Val, string $time2Val, bool $expected): void
+    {
+        self::assertEquals($expected, Time::fromString($time1Val)->lte(Time::fromString($time2Val)));
+    }
+
+    public function providerForTestGte(): array
+    {
+        return [
+            [
+                '01:00',
+                '01:00',
+                true,
+            ],
+            [
+                '01:00',
+                '01:01',
+                false,
+            ],
+            [
+                '01:00',
+                '00:59',
+                true,
+            ],
+        ];
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\Time::lte
+     * @dataProvider providerForTestGte
+     */
+    public function testGte(string $time1Val, string $time2Val, bool $expected): void
+    {
+        self::assertEquals($expected, Time::fromString($time1Val)->gte(Time::fromString($time2Val)));
+    }
 }
