89b34df3e2eae8dee8ea6df347b1600831a0fc2c
artarts36 <temicska99@mail.ru>
2023-05-25 00:11:16 +0300

    add tests for Time

--- tests/Unit/Shared/Time/TimeTest.php
+++ tests/Unit/Shared/Time/TimeTest.php
@@ -7,6 +7,14 @@ use ArtARTs36\MergeRequestLinter\Tests\TestCase;
 
 final class TimeTest extends TestCase
 {
+    public function providerForTestFromString(): array
+    {
+        return [
+            ['12:34', 12, 34],
+            ['00:59', 0, 59],
+        ];
+    }
+
     /**
      * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\Time::fromString
      * @dataProvider providerForTestFromString
@@ -27,11 +35,63 @@ final class TimeTest extends TestCase
         );
     }
 
-    public function providerForTestFromString(): array
+    public function providerForTestLte(): array
     {
         return [
-            ['12:34', 12, 34],
-            ['00:59', 0, 59],
+            [
+                '01:00',
+                '01:00',
+                true,
+            ],
+            [
+                '01:00',
+                '01:01',
+                true,
+            ],
+            [
+                '01:00',
+                '00:59',
+                false,
+            ],
         ];
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\Time::lte
+     * @dataProvider providerForTestLte
+     */
+    public function testLte(string $time1Val, string $time2Val, bool $expected): void
+    {
+        self::assertEquals($expected, Time::fromString($time1Val)->lte(Time::fromString($time2Val)));
+    }
+
+    public function providerForTestGte(): array
+    {
+        return [
+            [
+                '01:00',
+                '01:00',
+                true,
+            ],
+            [
+                '01:00',
+                '01:01',
+                false,
+            ],
+            [
+                '01:00',
+                '00:59',
+                true,
+            ],
+        ];
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Shared\Time\Time::lte
+     * @dataProvider providerForTestGte
+     */
+    public function testGte(string $time1Val, string $time2Val, bool $expected): void
+    {
+        self::assertEquals($expected, Time::fromString($time1Val)->gte(Time::fromString($time2Val)));
     }
 }
