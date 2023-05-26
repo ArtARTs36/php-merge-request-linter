fdbded6241ddb0dd40a2d78ad7c72a1e4f5ea6f9
artarts36 <temicska99@mail.ru>
2023-05-27 02:59:40 +0300

    up test

--- tests/Unit/Domain/Linter/LintResultTest.php
+++ tests/Unit/Domain/Linter/LintResultTest.php
@@ -4,26 +4,13 @@ namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Domain\Linter;
 
 use ArtARTs36\MergeRequestLinter\Domain\Linter\LintResult;
 use ArtARTs36\MergeRequestLinter\Domain\Linter\LintState;
+use ArtARTs36\MergeRequestLinter\Shared\DataStructure\Arrayee;
 use ArtARTs36\MergeRequestLinter\Shared\Time\Duration;
 use ArtARTs36\MergeRequestLinter\Tests\Mocks\EmptyNote;
 use ArtARTs36\MergeRequestLinter\Tests\TestCase;
 
 final class LintResultTest extends TestCase
 {
-    public function providerForTestIsFail(): array
-    {
-        return [
-            [
-                LintResult::fail(new EmptyNote(), new Duration(0.12)),
-                true,
-            ],
-            [
-                LintResult::successWithNote(new EmptyNote(), new Duration(0.13)),
-                false,
-            ],
-        ];
-    }
-
     /**
      * @covers \ArtARTs36\MergeRequestLinter\Domain\Linter\LintResult::successWithNote
      * @covers \ArtARTs36\MergeRequestLinter\Domain\Linter\LintResult::__construct
@@ -33,5 +20,15 @@ final class LintResultTest extends TestCase
         $result = LintResult::successWithNote(new EmptyNote(), new Duration(0.12));
 
         self::assertEquals(LintState::Success, $result->state);
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Domain\Linter\LintResult::fail
+     */
+    public function testFail(): void
+    {
+        $result = LintResult::fail(new EmptyNote(), new Duration(1));
+
+        self::assertEquals(LintState::Fail, $result->state);
     }
 }
