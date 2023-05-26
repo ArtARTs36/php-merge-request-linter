354ea6bb043d8d6328d958770b60ffbd209bb1e9
artarts36 <temicska99@mail.ru>
2023-05-26 17:20:08 +0300

    fix test

--- tests/Unit/Application/Linter/LinterTest.php
+++ tests/Unit/Application/Linter/LinterTest.php
@@ -4,6 +4,7 @@ namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Application\Linter;
 
 use ArtARTs36\MergeRequestLinter\Application\Linter\Linter;
 use ArtARTs36\MergeRequestLinter\Application\Rule\Definition\Definition;
+use ArtARTs36\MergeRequestLinter\Domain\Linter\LinterOptions;
 use ArtARTs36\MergeRequestLinter\Domain\Note\ExceptionNote;
 use ArtARTs36\MergeRequestLinter\Domain\Request\MergeRequest;
 use ArtARTs36\MergeRequestLinter\Domain\Rule\Rule;
@@ -21,27 +22,37 @@ final class LinterTest extends TestCase
      */
     public function testRunOnException(): void
     {
-        $linter = new Linter(new Rules([
-            new class () implements Rule {
-                public function getName(): string
-                {
-                    return 'anonymous_rule';
-                }
-
-                public function lint(MergeRequest $request): array
-                {
-                    throw new \RuntimeException();
-                }
-
-                public function getDefinition(): RuleDefinition
-                {
-                    return new Definition('');
-                }
-            },
-        ]), new NullEventDispatcher(), new NullMetricManager());
+        $linter = new Linter(
+            new Rules([
+                $this->createExceptionRule(),
+            ]),
+            new LinterOptions(false),
+            new NullEventDispatcher(),
+            new NullMetricManager(),
+        );
 
         $result = $linter->run($this->makeMergeRequest());
 
         self::assertInstanceOf(ExceptionNote::class, $result->notes->first());
+    }
+
+    private function createExceptionRule(): Rule
+    {
+        return new class () implements Rule {
+            public function getName(): string
+            {
+                return 'anonymous_rule';
+            }
+
+            public function lint(MergeRequest $request): array
+            {
+                throw new \RuntimeException();
+            }
+
+            public function getDefinition(): RuleDefinition
+            {
+                return new Definition('');
+            }
+        };
     }
 }
