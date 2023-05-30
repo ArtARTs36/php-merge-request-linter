26810a5b0ae440a315cc4d83f1789d46c168bcdd
artarts36 <temicska99@mail.ru>
2023-05-30 23:39:08 +0300

    update version

--- src/Version.php
+++ src/Version.php
@@ -8,7 +8,7 @@ namespace ArtARTs36\MergeRequestLinter;
  */
 final class Version
 {
-    public const VERSION = '0.14.1';
+    public const VERSION = '0.15.0';
 
     private function __construct()
     {
