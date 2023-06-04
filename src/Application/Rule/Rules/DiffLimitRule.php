6fafdbb3f00e64d98551bcc5ca0975e291ea4377
artarts36 <temicska99@mail.ru>
2023-06-04 16:39:40 +0300

    up

--- src/Application/Rule/Rules/DiffLimitRule.php
+++ src/Application/Rule/Rules/DiffLimitRule.php
@@ -4,6 +4,8 @@ namespace ArtARTs36\MergeRequestLinter\Application\Rule\Rules;
 
 use ArtARTs36\MergeRequestLinter\Application\Rule\Definition\Definition;
 use ArtARTs36\MergeRequestLinter\Domain\Note\LintNote;
+use ArtARTs36\MergeRequestLinter\Domain\Note\Note;
+use ArtARTs36\MergeRequestLinter\Domain\Request\Change;
 use ArtARTs36\MergeRequestLinter\Domain\Request\MergeRequest;
 use ArtARTs36\MergeRequestLinter\Domain\Rule\RuleDefinition;
 use ArtARTs36\MergeRequestLinter\Shared\Attributes\Description;
@@ -27,38 +29,51 @@ final class DiffLimitRule extends NamedRule
     public function lint(MergeRequest $request): array
     {
         $count = 0;
+        $notes = [];
+        $linesMaxExceeded = false;
 
         foreach ($request->changes as $change) {
             $fileLinesCount = $change->diff->changesCount();
             $count += $fileLinesCount;
 
             if ($this->fileLinesMax !== null && $fileLinesCount >= $this->fileLinesMax) {
-                return [
-                    new LintNote(sprintf(
-                        'Your request contains too many changes. The changed file "%s" must contain no more than %d changes.',
-                        $change->file,
-                        $this->fileLinesMax,
-                    )),
-                ];
+                $notes[] = $this->createNoteFileLinesLimitExceeded($change);
             }
 
-            if ($this->linesMax !== null && $count >= $this->linesMax) {
-                return [
-                    new LintNote(sprintf(
-                        'Your request contains too many changes. The request must contain no more than %d changes.',
-                        $this->linesMax,
-                    )),
-                ];
+            if (! $linesMaxExceeded && $this->linesMax !== null && $count >= $this->linesMax) {
+                if ($this->fileLinesMax === null) {
+                    return [$this->createNoteLinesMaxLimitExceeded()];
+                }
+
+                $notes[] = $this->createNoteLinesMaxLimitExceeded();
+                $linesMaxExceeded = true;
             }
         }
 
-        return [];
+        return $notes;
     }
 
     public function getDefinition(): RuleDefinition
     {
         return new Definition(sprintf(
             'The request must contain no more than %s changes.',
+            $this->linesMax,
+        ));
+    }
+
+    private function createNoteFileLinesLimitExceeded(Change $change): Note
+    {
+        return new LintNote(sprintf(
+            'Your request contains too many changes. The changed file "%s" must contain no more than %d changes.',
+            $change->file,
+            $this->fileLinesMax,
+        ));
+    }
+
+    private function createNoteLinesMaxLimitExceeded(): Note
+    {
+        return new LintNote(sprintf(
+            'Your request contains too many changes. The request must contain no more than %d changes.',
             $this->linesMax,
         ));
     }
