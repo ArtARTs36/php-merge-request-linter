ba51b67618dadb53f3273e2696883fc95a3a0970
artarts36 <temicska99@mail.ru>
2023-06-14 23:35:23 +0300

    fix test-docker command

--- tests/Mocks/MockGithubClient.php
+++ tests/Mocks/MockGithubClient.php
@@ -3,11 +3,15 @@
 namespace ArtARTs36\MergeRequestLinter\Tests\Mocks;
 
 use ArtARTs36\MergeRequestLinter\Domain\ToolInfo\Tag;
+use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Github\API\GraphQL\Input\AddCommentInput;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Github\API\GraphQL\Input\PullRequestInput;
+use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Github\API\GraphQL\Input\UpdateCommentInput;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Github\API\GraphQL\Type\PullRequest;
+use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Github\API\GraphQL\Type\Viewer;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Github\API\Rest\Tag\TagCollection;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Ci\System\Github\API\Rest\Tag\TagsInput;
 use ArtARTs36\MergeRequestLinter\Infrastructure\Contracts\CI\GithubClient;
+use ArtARTs36\MergeRequestLinter\Shared\DataStructure\Arrayee;
 
 final class MockGithubClient implements GithubClient
 {
@@ -28,5 +32,25 @@ final class MockGithubClient implements GithubClient
     public function getTags(TagsInput $input): TagCollection
     {
         return new TagCollection($this->tags);
+    }
+
+    public function postComment(AddCommentInput $input): string
+    {
+        // TODO: Implement postComment() method.
+    }
+
+    public function updateComment(UpdateCommentInput $input): void
+    {
+        // TODO: Implement updateComment() method.
+    }
+
+    public function getCurrentUser(string $graphqlUrl): Viewer
+    {
+        // TODO: Implement getCurrentUser() method.
+    }
+
+    public function getCommentsOnPullRequest(string $graphqlUrl, string $requestUri): Arrayee
+    {
+        // TODO: Implement getCommentsOnPullRequest() method.
     }
 }
--- tests/Unit/Application/Comments/CommenterFactoryTest.php
+++ tests/Unit/Application/Comments/CommenterFactoryTest.php
@@ -5,7 +5,12 @@ namespace ArtARTs36\MergeRequestLinter\Tests\Unit\Application\Comments;
 use ArtARTs36\MergeRequestLinter\Application\Comments\CommenterFactory;
 use ArtARTs36\MergeRequestLinter\Application\Comments\NullCommenter;
 use ArtARTs36\MergeRequestLinter\Domain\Configuration\CommentsPostStrategy;
+use ArtARTs36\MergeRequestLinter\Infrastructure\Text\Renderer\TwigRenderer;
+use ArtARTs36\MergeRequestLinter\Tests\Mocks\MockCi;
+use ArtARTs36\MergeRequestLinter\Tests\Mocks\MockCiSystemFactory;
+use ArtARTs36\MergeRequestLinter\Tests\Mocks\MockOperatorResolver;
 use ArtARTs36\MergeRequestLinter\Tests\TestCase;
+use Psr\Log\NullLogger;
 
 final class CommenterFactoryTest extends TestCase
 {
@@ -26,7 +31,14 @@ final class CommenterFactoryTest extends TestCase
      */
     public function testCreate(CommentsPostStrategy $strategy, string $expectedClass): void
     {
-        $factory = new CommenterFactory();
+        $factory = new CommenterFactory(
+            new MockCiSystemFactory(new MockCi([
+                'is_pull_request' => false,
+            ])),
+            new MockOperatorResolver(),
+            TwigRenderer::create(),
+            new NullLogger(),
+        );
 
         self::assertInstanceOf($expectedClass, $factory->create($strategy));
     }
