aea26a2da6edde5f438afd0817ee6a4a45a19526
artarts36 <temicska99@mail.ru>
2023-06-15 22:29:47 +0300

    add test for producer

--- src/Application/Comments/CommentProducer.php
+++ src/Application/Comments/CommentProducer.php
@@ -3,7 +3,7 @@
 namespace ArtARTs36\MergeRequestLinter\Application\Comments;
 
 use ArtARTs36\MergeRequestLinter\Application\Comments\Contracts\CommenterFactory;
-use ArtARTs36\MergeRequestLinter\Application\Comments\Message\MessageCreator;
+use ArtARTs36\MergeRequestLinter\Application\Comments\Contracts\CommentMessageCreator;
 use ArtARTs36\MergeRequestLinter\Domain\Configuration\CommentsConfig;
 use ArtARTs36\MergeRequestLinter\Domain\Linter\LintResult;
 use ArtARTs36\MergeRequestLinter\Domain\Request\MergeRequest;
@@ -12,7 +12,7 @@ use Psr\Log\LoggerInterface;
 final class CommentProducer implements Contracts\CommentProducer
 {
     public function __construct(
-        private readonly MessageCreator $messageCreator,
+        private readonly CommentMessageCreator $messageCreator,
         private readonly LoggerInterface $logger,
         private readonly CommenterFactory $commenterFactory,
     ) {
@@ -24,7 +24,10 @@ final class CommentProducer implements Contracts\CommentProducer
         $message = $this->messageCreator->create($request, $result, $config);
 
         if ($message === null) {
-            $this->logger->info('[CommentProducer] Comment message not found');
+            $this->logger->info(sprintf(
+                '[CommentProducer] Comment message for merge request with id "%s" not found',
+                $request->id,
+            ));
 
             return;
         }
@@ -33,5 +36,10 @@ final class CommentProducer implements Contracts\CommentProducer
             ->commenterFactory
             ->create($config->postStrategy)
             ->postComment($request, new MakingComment($message));
+
+        $this->logger->info(sprintf(
+            '[CommentProducer] Comment for merge request with id "%s" was sent',
+            $request->id,
+        ));
     }
 }
--- src/Application/Comments/Contracts/CommentProducer.php
+++ src/Application/Comments/Contracts/CommentProducer.php
@@ -2,11 +2,15 @@
 
 namespace ArtARTs36\MergeRequestLinter\Application\Comments\Contracts;
 
+use ArtARTs36\MergeRequestLinter\Application\Comments\Exceptions\SendCommentException;
 use ArtARTs36\MergeRequestLinter\Domain\Configuration\CommentsConfig;
 use ArtARTs36\MergeRequestLinter\Domain\Linter\LintResult;
 use ArtARTs36\MergeRequestLinter\Domain\Request\MergeRequest;
 
 interface CommentProducer
 {
+    /**
+     * @throws SendCommentException
+     */
     public function produce(MergeRequest $request, LintResult $result, CommentsConfig $config): void;
 }
