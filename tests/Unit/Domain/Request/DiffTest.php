87228d12c592563bbddf91c99f6beaa41040d8d9
artarts36 <temicska99@mail.ru>
2023-07-29 02:21:40 +0300

    Add tests for Diff

--- tests/Unit/Domain/Request/DiffTest.php
+++ tests/Unit/Domain/Request/DiffTest.php
@@ -11,6 +11,41 @@ use ArtARTs36\Str\Str;
 final class DiffTest extends TestCase
 {
     /**
+     * @covers \ArtARTs36\MergeRequestLinter\Domain\Request\Diff::empty
+     */
+    public function testEmpty(): void
+    {
+        $diff = Diff::empty();
+
+        self::assertTrue($diff->allFragments->isEmpty());
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Domain\Request\Diff::fromList
+     * @covers \ArtARTs36\MergeRequestLinter\Domain\Request\Diff::__construct
+     */
+    public function testFromList(): void
+    {
+        $diff = Diff::fromList([
+            new DiffFragment(DiffType::NEW, Str::fromEmpty()),
+            new DiffFragment(DiffType::NEW, Str::fromEmpty()),
+            new DiffFragment(DiffType::OLD, Str::fromEmpty()),
+            new DiffFragment(DiffType::OLD, Str::fromEmpty()),
+            new DiffFragment(DiffType::OLD, Str::fromEmpty()),
+            new DiffFragment(DiffType::NOT_CHANGES, Str::fromEmpty()),
+        ]);
+
+        self::assertEquals(
+            [2, 3, 1],
+            [
+                $diff->newFragments->count(),
+                $diff->oldFragments->count(),
+                $diff->notChangedFragments->count(),
+            ],
+        );
+    }
+
+    /**
      * @covers \ArtARTs36\MergeRequestLinter\Domain\Request\Diff::changesCount
      * @covers \ArtARTs36\MergeRequestLinter\Domain\Request\Diff::__construct
      */
