5f597ee256e78c17b8992a5e2f3097d7772f6874
artarts36 <temicska99@mail.ru>
2023-07-29 20:38:15 +0300

    add tests

--- tests/Unit/Infrastructure/Container/MapContainerTest.php
+++ tests/Unit/Infrastructure/Container/MapContainerTest.php
@@ -43,6 +43,19 @@ final class MapContainerTest extends TestCase
         $container->get('');
     }
 
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Infrastructure\Container\MapContainer::get
+     * @covers \ArtARTs36\MergeRequestLinter\Infrastructure\Container\MapContainer::bind
+     */
+    public function testGetOnBind(): void
+    {
+        $container = new MapContainer();
+
+        $container->bind(ClassForTestMapContainer::class, static fn () => new ClassForTestMapContainer());
+
+        self::assertInstanceOf(ClassForTestMapContainer::class, $container->get(ClassForTestMapContainer::class));
+    }
+
     public function providerForTestHas(): array
     {
         return [
@@ -73,6 +86,19 @@ final class MapContainerTest extends TestCase
         $container = new MapContainer($initial);
 
         self::assertEquals($expected, $container->has($class));
+    }
+
+    /**
+     * @covers \ArtARTs36\MergeRequestLinter\Infrastructure\Container\MapContainer::has
+     * @covers \ArtARTs36\MergeRequestLinter\Infrastructure\Container\MapContainer::bind
+     */
+    public function testHasOnBind(): void
+    {
+        $container = new MapContainer();
+
+        $container->bind(ClassForTestMapContainer::class, static fn () => new ClassForTestMapContainer());
+
+        self::assertTrue($container->has(ClassForTestMapContainer::class));
     }
 }
 
