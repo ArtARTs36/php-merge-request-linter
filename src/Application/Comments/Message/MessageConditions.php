c277b0171fa735668cf6b327632c7942fe2e26c5
artarts36 <temicska99@mail.ru>
2023-07-30 23:29:58 +0300

    add test

--- src/Application/Comments/Message/MessageConditions.php
+++ src/Application/Comments/Message/MessageConditions.php
@@ -5,6 +5,9 @@ namespace ArtARTs36\MergeRequestLinter\Application\Comments\Message;
 use ArtARTs36\MergeRequestLinter\Domain\Linter\LintResult;
 use ArtARTs36\MergeRequestLinter\Domain\Request\MergeRequest;
 
+/**
+ * @codeCoverageIgnore
+ */
 class MessageConditions
 {
     public function __construct(
