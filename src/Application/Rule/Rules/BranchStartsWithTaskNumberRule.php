a448f533a04b01bb836920d9e8a14bad4c6d6451
artarts36 <temicska99@mail.ru>
2023-08-06 20:42:34 +0300

    upd doc

--- src/Application/Rule/Rules/BranchStartsWithTaskNumberRule.php
+++ src/Application/Rule/Rules/BranchStartsWithTaskNumberRule.php
@@ -5,6 +5,7 @@ namespace ArtARTs36\MergeRequestLinter\Application\Rule\Rules;
 use ArtARTs36\MergeRequestLinter\Application\Rule\Definition\Definition;
 use ArtARTs36\MergeRequestLinter\Domain\Request\MergeRequest;
 use ArtARTs36\MergeRequestLinter\Domain\Rule\RuleDefinition;
+use ArtARTs36\MergeRequestLinter\Shared\Attributes\Example;
 
 /**
  * Source branch must starts with task number of project {projectName}. Mask: {projectName}-number
@@ -14,6 +15,7 @@ final class BranchStartsWithTaskNumberRule extends AbstractRule
     public const NAME = '@mr-linter/branch_starts_with_task_number';
 
     public function __construct(
+        #[Example('VIP')]
         private readonly string $projectName,
     ) {
         //
