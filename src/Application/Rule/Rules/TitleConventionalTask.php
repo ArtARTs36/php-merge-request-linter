309d9a1b5f24d00e7ea266baf810ff3d25871c1b
artarts36 <temicska99@mail.ru>
2023-09-02 18:36:34 +0300

    update docs

--- src/Application/Rule/Rules/TitleConventionalTask.php
+++ src/Application/Rule/Rules/TitleConventionalTask.php
@@ -2,6 +2,7 @@
 
 namespace ArtARTs36\MergeRequestLinter\Application\Rule\Rules;
 
+use ArtARTs36\MergeRequestLinter\Shared\Attributes\Description;
 use ArtARTs36\MergeRequestLinter\Shared\Attributes\Generic;
 use ArtARTs36\MergeRequestLinter\Shared\DataStructure\Arrayee;
 
@@ -14,6 +15,7 @@ readonly class TitleConventionalTask
      * @param Arrayee<int, string> $projectCodes
      */
     public function __construct(
+        #[Description('Project codes. Allowed empty list for any projects')]
         #[Generic(Generic::OF_STRING)]
         public Arrayee $projectCodes = new Arrayee([]),
     ) {
